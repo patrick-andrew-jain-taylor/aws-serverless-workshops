4f79ab0ef69d05e46ce3bed7463bceef68c74532
Patrick Jain-Taylor <pjaintaylor@eatonvance.com>
2020-06-05 15:22:17 -0400

    Testing with archives.

--- WebApplication/3_ServerlessBackend/aws_lambda_function.tf
+++ WebApplication/3_ServerlessBackend/aws_lambda_function.tf
@@ -1,8 +1,14 @@
+data "archive_file" "this" {
+  type = "zip"
+  source_file = "${path.module}/requestUnicorn.js"
+  output_path = "${path.module}/requestUnicorn.zip"
+}
+
 resource "aws_lambda_function" "this" {
   function_name = "RequestUnicorn"
   handler = "exports.handler"
   role = aws_iam_role.this.arn
   runtime = "nodejs10.x"
-  filename = "${path.module}/requestUnicorn.js"
-  source_code_hash = filebase64sha256("${path.module}/requestUnicorn.js")
+  filename = data.archive_file.this.output_path
+  source_code_hash = filebase64sha256(data.archive_file.this.output_path)
 }
