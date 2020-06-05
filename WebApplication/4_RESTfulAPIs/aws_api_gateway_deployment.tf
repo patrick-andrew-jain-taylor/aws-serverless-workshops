c601b0a8aae81faf065d19f2801a2dd8f5c3aab9
Patrick Jain-Taylor <pjaintaylor@eatonvance.com>
2020-06-05 16:04:52 -0400

    Adding API gateway deployment.

---
+++ WebApplication/4_RESTfulAPIs/aws_api_gateway_deployment.tf
@@ -0,0 +1,10 @@
+resource "aws_api_gateway_deployment" "this" {
+  rest_api_id = aws_api_gateway_rest_api.this.id
+  stage_name = "prod"
+
+  triggers = {
+    redeployment = sha1(join(",", list(
+      jsonencode(aws_api_gateway_integration.this),
+    )))
+  }
+}
