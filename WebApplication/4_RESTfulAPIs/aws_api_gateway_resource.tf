bb251d0a6ec3a0d0b6992fd6c21e3c17f822a463
Patrick Jain-Taylor <pjaintaylor@eatonvance.com>
2020-06-05 15:49:53 -0400

    Swapping IDs.

--- WebApplication/4_RESTfulAPIs/aws_api_gateway_resource.tf
+++ WebApplication/4_RESTfulAPIs/aws_api_gateway_resource.tf
@@ -1,5 +1,5 @@
 resource "aws_api_gateway_resource" "this" {
-  parent_id = aws_api_gateway_rest_api.this.id
+  parent_id = aws_api_gateway_rest_api.this.root_resource_id
   path_part = "ride"
-  rest_api_id = aws_api_gateway_rest_api.this.root_resource_id
+  rest_api_id = aws_api_gateway_rest_api.this.id
 }
