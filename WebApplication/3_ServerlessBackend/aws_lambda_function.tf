30ef638b9b5e13e7b9e926231185eea47bb80267
Patrick Jain-Taylor <pjaintaylor@eatonvance.com>
2020-06-05 15:14:03 -0400

    Adding Lambda function.

---
+++ WebApplication/3_ServerlessBackend/aws_lambda_function.tf
@@ -0,0 +1,7 @@
+resource "aws_lambda_function" "this" {
+  function_name = "RequestUnicorn"
+  handler = "exports.handler"
+  role = aws_iam_role.this.arn
+  runtime = "nodejs10.x"
+  source_code_hash = filebase64sha256("requestUnicorn.js")
+}
