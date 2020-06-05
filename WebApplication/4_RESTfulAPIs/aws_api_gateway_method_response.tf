2faf5050c3d21bf0e33bf7c5bc280df84ac31ad8
Patrick Jain-Taylor <pjaintaylor@eatonvance.com>
2020-06-05 16:37:51 -0400

    Removing empty.

--- WebApplication/4_RESTfulAPIs/aws_api_gateway_method_response.tf
+++ WebApplication/4_RESTfulAPIs/aws_api_gateway_method_response.tf
@@ -18,9 +18,6 @@ resource "aws_api_gateway_method_response" "post" {
   resource_id = aws_api_gateway_resource.this.id
   rest_api_id = aws_api_gateway_rest_api.this.id
   status_code = "200"
-  response_models = {
-    "application/json" = "Empty"
-  }
   response_parameters = {
     "method.response.header.Access-Control-Allow-Origin" = true
   }
