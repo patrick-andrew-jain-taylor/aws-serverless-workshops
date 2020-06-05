4dbbb0339b6a616001440f888585e995456803c5
Patrick Jain-Taylor <pjaintaylor@eatonvance.com>
2020-06-05 16:00:53 -0400

    Updating authorization.

--- WebApplication/4_RESTfulAPIs/aws_api_gateway_method.tf
+++ WebApplication/4_RESTfulAPIs/aws_api_gateway_method.tf
@@ -1,6 +1,7 @@
 resource "aws_api_gateway_method" "this" {
-  authorization = "NONE"
+  authorization = "COGNITO_USER_POOLS"
   http_method = "POST"
   resource_id = aws_api_gateway_resource.this.id
   rest_api_id = aws_api_gateway_rest_api.this.id
+  authorizer_id = aws_api_gateway_authorizer.this.id
 }
