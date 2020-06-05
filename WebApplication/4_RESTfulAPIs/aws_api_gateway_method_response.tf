2b81dc9c44811d9f9f5c9f30c47a24621dbd8149
Patrick Jain-Taylor <pjaintaylor@eatonvance.com>
2020-06-05 16:36:48 -0400

    Adding post method response.

--- WebApplication/4_RESTfulAPIs/aws_api_gateway_method_response.tf
+++ WebApplication/4_RESTfulAPIs/aws_api_gateway_method_response.tf
@@ -12,3 +12,16 @@ resource "aws_api_gateway_method_response" "options" {
     "method.response.header.Access-Control-Allow-Origin" = true
    }
 }
+
+resource "aws_api_gateway_method_response" "post" {
+  http_method = aws_api_gateway_method.post.http_method
+  resource_id = aws_api_gateway_resource.this.id
+  rest_api_id = aws_api_gateway_rest_api.this.id
+  status_code = "200"
+  response_models = {
+    "application/json" = "Empty"
+  }
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = true
+  }
+}
